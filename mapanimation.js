828281036a7ec3ef6892aa13b77c6fa6dab6b5a1
Jason On <onjjang@gmail.com>
2023-09-18 23:48:52 -0700

    add custom Marker

--- mapanimation.js
+++ mapanimation.js
@@ -16,7 +16,44 @@ const busStops = [
   
   // TODO: add your own access token
   mapboxgl.accessToken = 'pk.eyJ1Ijoib25qamFuZyIsImEiOiJjbGxiOXR0N3owNHZjM3BwaGZseXAycWpyIn0.ywqDH7R0aCDpjSkwhitHEA';
-  
+  const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
+    {
+    'type': 'Feature',
+    'properties': {
+    'message': 'Foo',
+    'iconSize': [60, 60]
+    },
+    'geometry': {
+    'type': 'Point',
+    'coordinates': [-66.324462, -16.024695]
+    }
+    },
+    {
+    'type': 'Feature',
+    'properties': {
+    'message': 'Bar',
+    'iconSize': [50, 50]
+    },
+    'geometry': {
+    'type': 'Point',
+    'coordinates': [-61.21582, -15.971891]
+    }
+    },
+    {
+    'type': 'Feature',
+    'properties': {
+    'message': 'Baz',
+    'iconSize': [40, 40]
+    },
+    'geometry': {
+    'type': 'Point',
+    'coordinates': [-63.292236, -18.281518]
+    }
+    }
+    ]
+    };
   // This is the map instance
   let map = new mapboxgl.Map({
     container: 'map',
@@ -26,8 +63,27 @@ const busStops = [
   });
   
   // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
-  let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
-  //let marker = new mapboxgl.marker().setLngLat([-71.092761, 42.357575]).addTo(map);
+  //let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
+  //for (const marker of geojson.features) {
+    // Create a DOM element for each marker.
+    const el = document.createElement('div');
+    const width = 60;
+    const height = 60;
+    el.className = 'marker';
+    el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
+    el.style.width = `${width}px`;
+    el.style.height = `${height}px`;
+    el.style.backgroundSize = '100%';
+     
+    //el.addEventListener('click', () => {
+    //window.alert(marker.properties.message);
+    //});
+     
+    // Add markers to the map.
+    let marker = new mapboxgl.Marker(el)
+    .setLngLat([-71.092761, 42.357575])
+    .addTo(map);
+   // }
   // counter here represents the index of the current bus stop
   let counter = 0;
   function move() {
